343bedc73645a415d5e661f84f84b816485f4a7d
Edward Cheadle <edward.cheadle@cambiahealth.com>
2018-05-04 15:26:08 -0600

    start/stop currently playing with cli and jq

--- instances/instances.sh
+++ instances/instances.sh
@@ -1,8 +1,18 @@
 # Describe all instances
 aws ec2 describe-instances  --profile ops
 
-aws ec2 describe-instances  --filters "Name=tag:Name,Values=chp-ops-server-rc-dev" --profile ops --query 'Reservations[*].Instances[*].[InstanceId]
+aws ec2 describe-instances  --profile ops \
+--filters "Name=tag:Name,Values=chp-ops-server-rc-prd"  \
+--query 'Reservations[*].Instances[*].[InstanceId]'
 
+aws ec2 describe-instances  --profile ops \
+--filters "Name=tag:Name,Values=chp-ops-server-rc-*" \
+--query 'Reservations[*].Instances[*]' | jq '.[][].InstanceId'
+
+aws ec2 describe-instances  --profile ops \
+ --filters "Name=tag:Name,Values=chp-ops-server-rc-dev" \
+   | jq '.[][]["Instances"]["VpcId","InstanceId"]'
+   
 start-instances
 --instance-ids <value>
 [--additional-info <value>]
--- instances/start.sh
+++ instances/start.sh
@@ -7,4 +7,6 @@
 # [--cli-input-json <value>]
 # [--generate-cli-skeleton <value>]
 
-aws ec2 
+aws ec2 start-instances --instance-ids i-0b2c7a86939138222  --dry-run
+
+aws ec2 stop-instances --instance-ids i-0b2c7a86939138222   --profile ops
