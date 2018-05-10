31ec08a69eb46d68f6b0b3c9bdf14f4dec326551
Edward Cheadle <edward.cheadle@cambiahealth.com>
2018-05-10 07:39:11 -0600

    Forgot to save before last commit.

--- instances/elb.sh
+++ instances/elb.sh
@@ -7,12 +7,14 @@ aws elb create-load-balancer --load-balancer-name tertra-classic-lb --listeners 
 aws elb create-load-balancer-listeners --load-balancer-name tertra-classic-lb \
 --listener "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80"
 
-aws elb create-load-balancer --load-balancer-name tetra-insternal-lb \
+aws elb create-load-balancer --load-balancer-name tetra-internal-lb \
 --listener "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80" \
 --scheme internal --subnets subnet-064941c694c919025 --security-groups sg-c6797cb8
 
-aws elb register-instances-with-loadbalancer --load-balancer-name tetra-insternal-lb \
+aws elb register-instances-with-loadbalancer --load-balancer-name tetra-internal-lb \
 --instance i-07496fa2047f3762e
 
-aws elb deregister-instances-from-loadbalancer --load-balancer-name tetra-insternal-lb \
+aws elb deregister-instances-from-loadbalancer --load-balancer-name tetra-internal-lb \
 --instance i-07496fa2047f3762e
+
+aws elb delete-load-balancer --load-balancer-name tetra-insternal-lb
