eee2a8801de822e62c9204ff76f47f51c1b49780
Edward Cheadle <edward.cheadle@cambiahealth.com>
2018-05-09 22:34:45 -0600

    Added additional elb commands

--- instances/elb.sh
+++ instances/elb.sh
@@ -5,5 +5,11 @@ aws elb create-load-balancer --load-balancer-name tertra-classic-lb --listeners 
 --subnets subnet-096156a471ca31d9b  --security-groups sg-c6797cb8
 
 aws elb create-load-balancer-listeners --load-balancer-name tertra-classic-lb \
+--listener "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80"
+
+aws elb create-load-balancer --load-balancer-name tetra-insternal-lb \
 --listener "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80" \
 --scheme internal --subnets subnet-064941c694c919025 --security-groups sg-c6797cb8
+
+aws elb register-instances-with-loadbalancer --load-balancer-name tetra-insternal-lb \
+--instance i-07496fa2047f3762e
